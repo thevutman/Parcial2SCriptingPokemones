7ab2a8d44f9876b08a7db91f7751477bde5c1199
Thevutman <santiagovelascomosquera@hotmail.com>
2025-09-24 06:11:38 -0500

    Agregar archivos de proyecto.

---
+++ Parcial2SCriptingPokemones/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Parcial2SCriptingPokemones
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
